73f5d16380dec115bbccea81d7dafa8a21a9edb5
AnnaAmalfi94 <annamalfi@gmail.com>
2017-01-29 17:36:33 +0100

    Add files via upload

--- themes/my-theme/js/functions.js
+++ themes/my-theme/js/functions.js
@@ -17,25 +17,6 @@ define(['jquery','core/theme-app','core/theme-tpl-tags','core/modules/storage','
    
    
    
-  
-			document.addEventListener('deviceready', function () {
-  // Enable to debug issues.
-  // window.plugins.OneSignal.setLogLevel({logLevel: 4, visualLevel: 4});
-  
-  var notificationOpenedCallback = function(jsonData) {
-    console.log('notificationOpenedCallback: ' + JSON.stringify(jsonData));
-  };
-
-  window.plugins.OneSignal
-    .startInit("74dacbd8-af1e-4d05-aabf-fb85cb2804c7", "905211826236")
-    .handleNotificationOpened(notificationOpenedCallback)
-    .endInit();
-  
-  // Call syncHashedEmail anywhere in your app if you have the user's email.
-  // This improves the effectiveness of OneSignal's "best-time" notification scheduling feature.
-  // window.plugins.OneSignal.syncHashedEmail(userEmail);
-}, false);
-		
    
    
    
