9e407d0d08b1af9e11c15855f904c685f545f1d9
AnnaAmalfi94 <annamalfi@gmail.com>
2017-02-09 18:23:13 +0100

    Update functions.js

--- themes/my-theme/js/functions.js
+++ themes/my-theme/js/functions.js
@@ -44,6 +44,28 @@ define(['jquery','core/theme-app','core/theme-tpl-tags','core/modules/storage','
    
    
    
+	document.addEventListener("deviceready", onDeviceReady, false);
+
+function onDeviceReady(){
+    document.addEventListener("backbutton", function(e){
+       if($.mobile.activePage.is('#homepage')){
+           e.preventDefault();
+           navigator.app.exitApp();
+       }
+       else {
+           navigator.app.backHistory()
+       }
+    }, false);
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
    
    
    
